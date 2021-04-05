c580b0e9a23a0799137f72c6741c74f65e847af3
JustinasLekavicius <justinas.l@superhow.io>
2021-04-05 20:44:16 +0300

    Removed some unused UI elements, updated network configuration file.

--- src/views/PageAssembler.vue
+++ src/views/PageAssembler.vue
@@ -3,15 +3,13 @@
 		<div class="page" v-if="layout === 'flex'">
 			<div class="page-content-card-f" :class="layoutOptions">
 				<template v-for="(item, index) in schema">
-					<component
-						:is="item.type"
-						v-if="isItemShown(item)"
-						class="card-f"
-						:class="{'card-full-width': item.layoutOptions === 'full-width', 'card-adaptive': item.layoutOptions === 'adaptive'}"
-						:data-cy="item.title"
-						v-bind="item"
-						:key="'col' + item.title + index"
-					/>
+					<component :is="item.type"
+										 v-if="isItemShown(item)"
+										 class="card-f"
+										 :class="{'card-full-width': item.layoutOptions === 'full-width', 'card-adaptive': item.layoutOptions === 'adaptive'}"
+										 :data-cy="item.title"
+										 v-bind="item"
+										 :key="'col' + item.title + index" />
 				</template>
 			</div>
 		</div>
@@ -19,27 +17,23 @@
 			<b-row class="my-4 mx-0 mx-xs-0 mx-md-4 mx-lg-8" :class="layoutOptions">
 				<template v-for="(item, index) in schema">
 					<b-col :class="item.layoutOptions" class="bootstrap-col" :key="'col' + item.title + index">
-						<component
-							:is="item.type"
-							v-if="isItemShown(item)"
-							v-bind="item"
-							style="width: 100%"
-							:key="item.title + index"
-						/>
+						<component :is="item.type"
+											 v-if="isItemShown(item)"
+											 v-bind="item"
+											 style="width: 100%"
+											 :key="item.title + index" />
 					</b-col>
 				</template>
 			</b-row>
 		</b-container>
 		<div class="grid" v-else-if="layout === 'grid'">
 			<template v-for="(item, index) in schema">
-				<component
-					:is="item.type"
-					v-if="isItemShown(item)"
-					style="margin-bottom: 0"
-					:style="item.layoutOptions"
-					v-bind="item"
-					:key="'col' + item.title + index"
-				/>
+				<component :is="item.type"
+									 v-if="isItemShown(item)"
+									 style="margin-bottom: 0"
+									 :style="item.layoutOptions"
+									 v-bind="item"
+									 :key="'col' + item.title + index" />
 			</template>
 		</div>
 	</div>
@@ -54,7 +48,6 @@ import RecentTransactionsWidget from '@/components/widgets/RecentTransactionsWid
 import TransactionGraphicWidget from '@/components/widgets/TransactionGraphicWidget.vue';
 import AccountBalanceWidget from '@/components/widgets/AccountBalanceWidget.vue';
 import NodeRewardsClient from '@/components/widgets/NodeRewardsWidget';
-import NodesMapWidget from '@/components/widgets/NodesMapWidget.vue';
 import NodeStatsWidget from '@/components/widgets/NodeStatsWidget.vue';
 
 export default {
@@ -66,7 +59,6 @@ export default {
 		RecentTransactionsWidget,
 		TransactionGraphicWidget,
 		AccountBalanceWidget,
-		NodesMapWidget,
 		NodeRewardsClient,
 		NodeStatsWidget
 	},
@@ -156,73 +148,75 @@ export default {
 </script>
 
 <style lang="scss">
-.page-assembler {
-    width: 100%;
-    height: 100%;
-}
+	.page-assembler {
+		width: 100%;
+		height: 100%;
+	}
 
-.bootstrap-col {
-    display: flex;
-    display: -webkit-flex;
-    flex-wrap: wrap;
-}
+	.bootstrap-col {
+		display: flex;
+		display: -webkit-flex;
+		flex-wrap: wrap;
+	}
 
-.grid {
-    margin: 20px;
-    height: 100%;
-    display: grid;
-    grid-gap: 20px;
-}
+	.grid {
+		margin: 20px;
+		height: 100%;
+		display: grid;
+		grid-gap: 20px;
+	}
 
-.page {
-    .page-content-card-f {
-        padding: 10px;
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-    }
+	.page {
+		.page-content-card-f
 
-    .card-f {
-        margin: 10px;
-    }
+	{
+		padding: 10px;
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+	}
 
-    .card-adaptive {
-        flex: 1 1 auto;
-        max-width: 100%;
-    }
+	.card-f {
+		margin: 10px;
+	}
 
-    .card-full-width {
-        width: 100%;
-    }
+	.card-adaptive {
+		flex: 1 1 auto;
+		max-width: 100%;
+	}
 
-    .max-height-medium {
-        max-height: 100px;
-    }
+	.card-full-width {
+		width: 100%;
+	}
 
-    @media screen and (max-width: 40em) {
-        .page-content-card-f {
-            padding-left: 0;
-            padding-right: 0;
-        }
+	.max-height-medium {
+		max-height: 100px;
+	}
 
-        .card-f {
-            padding: 5px;
-            padding-top: 10px;
-            margin-left: 0;
-            margin-right: 0;
-            width: 100%;
-        }
-    }
+	@media screen and (max-width: 40em) {
+		.page-content-card-f {
+			padding-left: 0;
+			padding-right: 0;
+		}
 
-    @media screen and (min-width: 80em) {
-        .page-content-card-f {
-            padding-left: 80px;
-            padding-right: 80px;
-        }
+		.card-f {
+			padding: 5px;
+			padding-top: 10px;
+			margin-left: 0;
+			margin-right: 0;
+			width: 100%;
+		}
+	}
 
-        .card-f {
-            margin-right: 10px;
-        }
-    }
-}
+	@media screen and (min-width: 80em) {
+		.page-content-card-f {
+			padding-left: 80px;
+			padding-right: 80px;
+		}
+
+		.card-f {
+			margin-right: 10px;
+		}
+	}
+	}
 </style>
